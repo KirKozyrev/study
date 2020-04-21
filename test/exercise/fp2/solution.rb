eb00c6f75bb8b3c7491f380a3c4530fb702739a1
KirKozyrev <kozyrev.k1995@gmail.com>
2020-04-21 23:41:49 +0400

    Added functions

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -6,18 +6,50 @@ module Exercise
 
       # Написать свою функцию my_each
       def my_each
+        i = 0
+        while i < self.length do
+          yield self[i]
+          i += 1;
+        end
+
+        return self
       end
 
       # Написать свою функцию my_map
       def my_map
+        result = MyArray.new
+
+        my_each do |item|
+          result.push(yield(item))
+        end
+
+        result
       end
 
       # Написать свою функцию my_compact
       def my_compact
+        result = MyArray.new
+
+        my_each do |item| 
+          result.push(item) if item != nil
+        end
+
+        result
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce
+      def my_reduce(init = nil)
+        result = init
+
+        my_each do |item|
+          if result.nil?
+            result = item
+          else
+            result = yield(result, item)
+          end
+        end
+
+        result
       end
     end
   end
