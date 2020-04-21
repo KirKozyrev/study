cb9a2fae192b5c7335cb2bfd4bbd032e50246d0d
KirKozyrev <kozyrev.k1995@gmail.com>
2020-04-21 13:47:40 +0400

    Edit functions

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,10 +2,14 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        max = array.max
+        max = 0
         newarr = []
 
-        for item in array do
+        array.each do |item|
+          max = item if item > max
+        end
+
+        array.each do |item|
           if item > 0
             newarr.push(max)
           else
@@ -13,40 +17,28 @@ module Exercise
           end
         end
 
-        return newarr
+        newarr
       end
 
-      def search(_array, _query)
-        if _array.length == 0
-          return -1
+      def search(array, query, *array_range)
+        return -1 if array.empty?
+        return 0 if array.length == 1 && array[0] == query
+        return -1 if array.length == 1
+
+        arr_start = array_range[0] || 0
+        arr_end = array_range[1] || array.length
+
+        return -1 if arr_start >= arr_end
+
+        arr_middle = arr_start + (arr_end - arr_start) / 2
+
+        return arr_middle if array[arr_middle] == query
+
+        if array[arr_middle] > query 
+          search(array, query, arr_start, arr_middle)
+        else
+          search(array, query, arr_middle + 1, arr_end)
         end
-
-        if _array.length == 1 && _array[0] == _query
-          return 0
-        end
-
-        if _array.length == 1
-          return -1
-        end
-
-        arr_start = 0
-        arr_end = _array.length
-
-        while arr_start < arr_end do
-          arr_middle = arr_start + (arr_end - arr_start) / 2
-
-          if _array[arr_middle] == _query
-            return arr_middle
-          end
-
-          if _array[arr_middle] > _query 
-            arr_end = arr_middle
-          else
-            arr_start = arr_middle + 1
-          end
-        end
-
-        return -1
       end
     end
   end
