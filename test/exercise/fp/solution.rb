4c13f67d64154c14c3b98caa2a70f1e722b42748
KirKozyrev <kozyrev.k1995@gmail.com>
2020-04-23 15:29:24 +0400

    Add nil? method, change reduce

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,32 +4,25 @@ module Exercise
       def rating(films)
         films_rates = 
           films.select { |film| 
-            (film["rating_kinopoisk"] != nil && film["rating_kinopoisk"].to_f > 0) && 
-            (film["country"] != nil && film["country"].split(",").length > 1)
+            (!film["rating_kinopoisk"].nil? && film["rating_kinopoisk"].to_f > 0) && 
+            (!film["country"].nil? && film["country"].split(",").length > 1)
           }.map { |film|
             film["rating_kinopoisk"].to_f
           }
         
-        rates_sum = films_rates.reduce do |sum, rate| 
-          sum + rate
-        end
-        
+        rates_sum = films_rates.reduce(0, :+)
         rates_sum /= films_rates.length
       end
 
       def chars_count(films, threshold)
         count_char = 
           films.select { |film| 
-            film["rating_kinopoisk"] != nil && film["rating_kinopoisk"].to_f >= threshold
+            !film["rating_kinopoisk"].nil? && film["rating_kinopoisk"].to_f >= threshold
           }.map { |film|
             film["name"].count('и')
           }
             
-        char_sum = count_char.reduce do |sum, item| 
-          sum + item
-        end
-
-        char_sum
+        char_sum = count_char.reduce(0, :+)
       end
     end
   end
