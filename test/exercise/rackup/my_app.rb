a8d36d9f15bfbea6076821ef80ad143abdb10deb
KirKozyrev <kozyrev.k1995@gmail.com>
2020-04-29 17:54:50 +0400

    new responses

--- test/exercise/rackup/my_app.rb
+++ test/exercise/rackup/my_app.rb
@@ -1,5 +1,13 @@
 Inatra.routes do
-  post '/hello' do
+  get '/hello' do
     [200, {}, ['Hello World']]
+  end
+
+  get '/ping' do
+    [200, {}, ['PONG']]
+  end
+
+  post '/bye' do
+    [200, {}, ['Bye Bye']]
   end
 end
