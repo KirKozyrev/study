1147772e2109bc97be3ea26c3511bd14712ef86b
KirKozyrev <kozyrev.k1995@gmail.com>
2020-04-29 18:40:17 +0400

    Change method_missing, call

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -5,13 +5,22 @@ module Inatra
       instance_eval(&block)
     end
 
-    def method_missing(method, *args, &block)
-      @route[method.to_s] = {}
-      @route[method.to_s][args[0]] = block
+    def method_missing(method, arg, &block)
+      method = method.to_s
+
+      @route.store(method, {}) if @route[method].nil?
+      @route[method].store(arg, block)
     end
 
     def call(env)
-      @route[env['REQUEST_METHOD'].downcase][env['PATH_INFO']].call
+      method = env['REQUEST_METHOD'].to_s.downcase
+      path = env['PATH_INFO'].to_s.downcase
+
+      if @route[method][path].nil?
+        return [404, {}, 'Not Found']
+      else
+        @route[method][path].call
+      end
     end
   end
 end
